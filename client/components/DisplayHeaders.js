f72991bc0d8c099e0b34d9dffb877fec44414bcb
mlampedx <mlampesd@gmail.com>
2017-01-14 21:39:49 -0800

    Began redesign of DisplayHeaders component

--- client/components/DisplayHeaders.js
+++ client/components/DisplayHeaders.js
@@ -1,35 +1,61 @@
 const React = require('react');
+import FlatButton from 'material-ui/FlatButton';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 const DisplayHeaders = ({toggleCategory, activeCategory}) => {
   return (
-    <div>
-      <span>
-        <button 
-          className='pol-btn'
-          onClick = {() => toggleCategory('Politics')}>
-          Politics
-        </button>
-        <button 
-          className='econ-btn' 
-          onClick = {() => toggleCategory('Economics')}>
-          Economics
-        </button>
-        <button 
-          className='soc-btn' 
-          onClick = {() => toggleCategory('Society')}>
-          Society
-        </button>
-        <button 
-          className='geo-btn'
-          onClick = {() => toggleCategory('Geopolitics')}>
-          Geopolitics
-        </button>
-      </span>
-    <div>
-      <div className='data-label'>Qualitative Data</div>
-    </div>
-    <hr />
-    </div>
+    <Tabs>
+      <Tab
+        className = 'pol-btn'
+        label = 'Politics'
+        onClick = {() => toggleCategory('Politics')}
+      />
+      <Tab
+        className = 'econ-btn'
+        label = 'Economics'
+        onClick = {() => toggleCategory('Economics')}
+      />
+      <Tab
+        className = 'soc-btn'
+        label = 'Society'
+        onClick = {() => toggleCategory('Society')}
+      />
+      <Tab
+        className = 'geo-btn'
+        label = 'Geopolitics'
+        onClick = {() => toggleCategory('Geopolitics')}
+      />
+      
+    </Tabs>
+
+    // <div>
+    //   <span>
+    //     <FlatButton 
+    //       className='pol-btn'
+    //       onClick = {() => toggleCategory('Politics')}
+    //       label = 'Politics'
+    //     />
+    //     <FlatButton 
+    //       className='econ-btn' 
+    //       onClick = {() => toggleCategory('Economics')}
+    //       label = 'Economics'
+    //     />
+    //     <FlatButton 
+    //       className='soc-btn' 
+    //       onClick = {() => toggleCategory('Society')}
+    //       label = 'Society'
+    //     />
+    //     <FlatButton 
+    //       className='geo-btn'
+    //       onClick = {() => toggleCategory('Geopolitics')}
+    //       label = 'Geopolitics'
+    //     />
+    //   </span>
+    // <div>
+    //   <div className='data-label'>Qualitative Data</div>
+    // </div>
+    // <hr />
+    // </div>
   )
 }
 
