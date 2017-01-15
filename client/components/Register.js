22f1777e211c390a5a5221fc2f1b868375e95586
mlampedx <mlampesd@gmail.com>
2017-01-14 21:08:07 -0800

    Redesigned register component

--- client/components/Register.js
+++ client/components/Register.js
@@ -1,20 +1,59 @@
 import React, {PropTypes} from 'react';
 import { Link } from 'react-router';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const Register = ({registerUser}) => {
+
   return (
-    <div className='registration-form'>
-      <h1>Register an Account for Globetrotter</h1>
-      <form onSubmit={registerUser}>
-        <ul>
-          <li><label>Username:</label><input name="username" type="text" placeholder="username"></input></li>
-          <li><label>Email:</label><input name="email" type="email" placeholder="email"></input></li>
-          <li><label>Password:</label><input name="password" type="password" placeholder="password"></input></li>
-          <li><label>Confirm Password:</label><input name="password2" type="password" placeholder="confirm password"></input></li>
-          <button type="submit" className="btn btn-primary">Register</button>
-        </ul>
-        <Link to="/" className="btn btn-primary">Back</Link>
-      </form>
+    <div className = 'login-div'>
+      <Paper className = 'login-form' zDepth = {3}>
+        <h1>Register an Account</h1>
+        <form onSubmit={registerUser}>
+          <span>
+            <TextField 
+              className = 'login-field'
+              hintText = 'Username'
+              name = 'username'
+              type = 'text' 
+            />
+            <TextField 
+              className = 'login-field'
+              hintText = 'Email'
+              name = 'email'
+              type = 'email'
+            />
+            <TextField 
+              className = 'login-field'
+              hintText = 'Password'
+              name = 'password'
+              type = 'password'
+            />
+            <TextField 
+              className = 'login-field'
+              hintText = 'Confirm Password'
+              name = 'password2'
+              type = 'password'
+            />
+            <RaisedButton 
+              className = "login-btn"
+              label = 'Register'
+              secondary = { true }
+              type = "submit" 
+            >
+            </RaisedButton>
+            <Link to="/">
+              <RaisedButton 
+                className = "login-btn"
+                label = 'Login'
+                primary = { true }
+              >
+              </RaisedButton>
+            </Link>
+          </span>
+        </form>
+      </Paper>
     </div>
   );
 };
