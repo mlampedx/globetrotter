bfe4d8a7e2207ccd961d25cc42aa11e92701bb56
mlampedx <mlampesd@gmail.com>
2017-01-15 20:38:02 -0800

    Removed deprecated components

--- client/components/index.js
+++ client/components/index.js
@@ -1,13 +1,11 @@
 export { default as Container } from './Container';
 export { default as CountrySelector } from './CountrySelector';
-export { default as DataColumn } from './DataColumn';
+export { default as DataDashboard } from './DataDashboard';
 export { default as Display } from './Display';
 export { default as DisplayHeaders } from './DisplayHeaders';
-export { default as FavoriteDataRow } from './FavoriteDataRow';
+export { default as FavoritesDashboard } from './FavoritesDashboard';
 export { default as Login } from './Login';
 export { default as Nav } from './Nav';
 export { default as Profile } from './Profile';
-export { default as QualDataRow } from './QualDataRow';
-export { default as QuantDataRow } from './QuantDataRow';
 export { default as Register } from './Register';
 export { default as Welcome } from './Welcome';
