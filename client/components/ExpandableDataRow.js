c06c1ca11f5427ba3230fca9b0bc2d4ac1b30570
mlampedx <mlampesd@gmail.com>
2017-01-25 20:52:56 -0800

    Created modal window to display full text of indicators

---
+++ client/components/ExpandableDataRow.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default class ExpandableDataRow extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        open: false,
+      };
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleOpen() {
+    this.setState({open: true});
+  };
+
+  handleClose() {
+    this.setState({open: false});
+  };
+
+  render() {
+    console.log('rendering')
+    const actions = [
+      <RaisedButton
+        label="Close"
+        primary={true}
+        onTouchTap={this.handleClose}
+        onClick={this.handleClose}
+      />,
+    ];
+
+    return (
+      <div>
+        <FlatButton
+          label="View Full Text"
+          secondary={true}
+          onTouchTap={this.handleOpen}
+          onClick={this.handleOpen}
+        />
+        <Dialog
+          title= {this.props.name}
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          { this.props.value }
+        </Dialog>
+      </div>
+    );
+  }
+}
